1a9f85cfddd8ed73b32ff92aa8e84b67566c3ad6
dmking0728 <dmking0728@gmail.com>
2018-06-30 15:23:36 +0200

    minor adjustments in responsiveness

--- source/js/app.js
+++ source/js/app.js
@@ -11,9 +11,48 @@ function showNav(){
 // JavaScript
 window.sr = ScrollReveal();
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px'
-// });
+sr.reveal('#header img.phone', {
+    delay: 0,
+    duration: 800,
+    opacity: 0,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.car', {
+    delay: 200,
+    duration: 300,
+    opacity: 0,
+    origin: 'top',
+    distance: '100px'
+});
+sr.reveal('.shoe', {
+    delay: 400,
+    duration: 300,
+    opacity: 0,
+    origin: 'top',
+    distance: '100px'
+});
+sr.reveal('.device', {
+    delay: 600,
+    duration: 300,
+    opacity: 0,
+    origin: 'top',
+    distance: '100px'
+});
+
+sr.reveal('.discount-layout img', {
+    delay: 100,
+    duration: 600,
+    opacity: 0,
+    origin: 'left',
+    distance: '100px'
+});
+
+sr.reveal('.offer', {
+    delay: 100,
+    duration: 600,
+    opacity: 0,
+    origin: 'right',
+    distance: '300px'
+});
